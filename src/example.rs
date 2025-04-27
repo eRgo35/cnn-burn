8c0f19483e9dca65ed2c1664c41170812dde7814
Michał Czyż <mike@c2yz.com>
2025-04-27 22:49:21 +0200

    example simple nn

---
+++ src/example.rs
@@ -0,0 +1,14 @@
+use burn::tensor::{Tensor, backend::Backend};
+
+fn computation<B: Backend>() {
+    let device = Default::default();
+
+    let tensor1: Tensor<B, 2> = Tensor::from_floats([[2., 3.], [4., 5.]], &device);
+    let tensor2 = Tensor::ones_like(&tensor1);
+
+    println!("{:}", tensor1 + tensor2);
+}
+
+fn main() {
+    computation::<burn::backend::Wgpu>();
+}
