f0246d0c293b8fbabc58dac6105883c0a2306ec8
Michał Czyż <mike@c2yz.com>
2025-04-27 22:47:47 +0200

    tutorial sync

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use burn::backend::Wgpu;
+use model::ModelConfig;
+
+mod data;
+mod model;
+mod training;
+
+fn main() {
+    type MyBackend = Wgpu<f32, i32>;
+
+    let device = Default::default();
+    let model = ModelConfig::new(10, 512).init::<MyBackend>(&device);
+
+    println!("{}", model);
+}
---
+++ src/training.rs
@@ -0,0 +1,72 @@
+use crate::{
+    data::{MnistBatch, MnistBatcher},
+    model::{Model, ModelConfig},
+};
+use burn::{
+    nn::loss::CrossEntropyLossConfig,
+    optim::AdamConfig,
+    prelude::*,
+    tensor::backend::AutodiffBackend,
+    train::{ClassificationOutput, TrainOutput, TrainStep, ValidStep},
+};
+
+impl<B: Backend> Model<B> {
+    pub fn forward_classification(
+        &self,
+        images: Tensor<B, 3>,
+        targets: Tensor<B, 1, Int>,
+    ) -> ClassificationOutput<B> {
+        let output = self.forward(images);
+        let loss = CrossEntropyLossConfig::new()
+            .init(&output.device())
+            .forward(output.clone(), targets.clone());
+
+        ClassificationOutput::new(loss, output, targets)
+    }
+}
+
+impl<B: AutodiffBackend> TrainStep<MnistBatch<B>, ClassificationOutput<B>> for Model<B> {
+    fn step(&self, batch: MnistBatch<B>) -> TrainOutput<ClassificationOutput<B>> {
+        let item = self.forward_classification(batch.images, batch.targets);
+
+        TrainOutput::new(self, item.loss.backward(), item)
+    }
+}
+
+impl<B: Backend> ValidStep<MnistBatch<B>, ClassificationOutput<B>> for Model<B> {
+    fn step(&self, batch: MnistBatch<B>) -> ClassificationOutput<B> {
+        self.forward_classification(batch.images, batch.targets)
+    }
+}
+
+#[derive(Config)]
+pub struct TrainingConfig {
+    pub model: ModelConfig,
+    pub optimizer: AdamConfig,
+    #[config(default = 10)]
+    pub num_epochs: usize,
+    #[config(default = 64)]
+    pub batch_size: usize,
+    #[config(default = 4)]
+    pub num_workers: usize,
+    #[config(default = 42)]
+    pub seed: u64,
+    #[config(default = 1.0e-4)]
+    pub learning_rate: f64,
+}
+
+fn create_artifact_dir(artifact_dir: &str) {
+    std::fs::remove_dir_all(artifact_dir).ok();
+    std::fs::create_dir_all(artifact_dir).ok();
+}
+
+pub fn train<B: AutodiffBackend>(artifact_dir: &str, config: TrainingConfig, device: B::Device) {
+    create_artifact_dir(artifact_dir);
+    config
+        .save(format!("{artifact_dir}/config.json"))
+        .expect("Config should be saved successfully");
+
+    B::seed(config.seed);
+
+    let batcher = MnistBatcher::default();
+}
